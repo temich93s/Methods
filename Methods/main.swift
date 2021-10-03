76679b1ed5cc712a7477e4a8592d60ef959493f0
Temich93s <nil>
2021-10-03 13:08:43 +1000

    Пройдена тема: Методы экземпляра

--- Methods/main.swift
+++ Methods/main.swift
@@ -9,3 +9,34 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: Методы экземпляра
+print("\n//Методы экземпляра")
+
+class Counter {
+    var count = 0
+    func increment() {
+        count += 1
+    }
+    func increment(by amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+
+let counter = Counter()
+print(counter.count)
+// начальное значение counter равно 0
+
+counter.increment()
+print(counter.count)
+// теперь значение counter равно 1
+
+counter.increment(by: 5)
+print(counter.count)
+// теперь значение counter равно 6
+
+counter.reset()
+print(counter.count)
+// теперь значение counter равно 0
